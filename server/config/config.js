58531247bacda429baf166cdfbaf7b22438223e8
hyeongirlife <leehyeongul@gmail.com>
2022-03-18 14:30:17 +0900

    [Deploy]_Feat_서버배포를위한 환경설정

---
+++ server/config/config.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  "development": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "timezone": "+09:00"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "timezone": "+09: 00"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "timezone": "+09: 00"
+  }
+}
