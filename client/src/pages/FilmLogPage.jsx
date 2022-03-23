cf18bbbc09a88246ad1b98237693dd7a25321e8a
SEB_Full_36기_서외구 <89363048+sud665@users.noreply.github.com>
2022-03-23 14:10:36 +0900

    [Front]_Design_기능추가에따른 컴포넌트,페이지 수정

---
+++ client/src/pages/FilmLogPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './GalleryPage.css';
+
+function GalleryPage() {
+  /* TODO : 갤러리 페이지 만들기. */
+
+  return (
+    <section className="gallerypage-section">
+      
+    </section>
+  );
+}
+
+export default GalleryPage;
