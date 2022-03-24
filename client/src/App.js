0993ea547feeba7ad9a2a0fe72a006c3ca92e150
SEB_Full_36기_서외구 <89363048+sud665@users.noreply.github.com>
2022-03-24 09:23:19 +0900

    [Front]_Design_app파일 비움

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,10 @@
 import "./App.css";
-// import styled from "styled-components";
-import FilmLogPage from "./pages/FilmLogPage";
 
-// const Title = styled.h1`
-//   padding: 20px;
-//   color: tomato;
-//   text-align: center;
-// `;
 
 function App() {
   return (
     <div>
-      <FilmLogPage />
+      
     </div>
   );
 }
