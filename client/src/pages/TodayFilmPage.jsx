263e63d6d77abe4f40182aaa42cba1487061b073
SEB_Full_36기_서외구 <89363048+sud665@users.noreply.github.com>
2022-03-28 00:43:58 +0900

    [Front]_design_오늘의 필름 페이지 레이지아웃

--- client/src/pages/TodayFilmPage.jsx
+++ client/src/pages/TodayFilmPage.jsx
@@ -1,16 +1,37 @@
 /* TODO : 오늘의 필름 페이지 입니다. */
 import React from "react";
 import styled from "styled-components";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import {  faCloudHail } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function TodayFilmPage() {
   return (
     <>
       <Container>
-        
-        <h3>오늘의 필름 페이지 입니다.</h3>
-        <FontAwesomeIcon icon="fa-light fa-cloud-hail" />
+        <h2>현재 위치의 날씨에 따라 적합한 필름을 추천해 드려요!</h2>
+        <FontAwesomeIcon icon={faSearch} className="search" />
+        <h3>
+          오늘의 날씨는 흐림
+          <br />
+          어두운 환경에서는 감도가 높은 필름을 추천해드려요.
+        </h3>
+        <Section>
+          <FilmBox>
+            <img className="filmimg" src="https://user-images.githubusercontent.com/87605663/159632005-c03708e7-9d27-411e-b0c3-b0d32f68f186.png" alt='film'/>
+            <h3 className="filmtitle">코닥400</h3>
+            <p className="filminfo">필름설명이 필요한곳 입니다.</p>
+          </FilmBox>
+          <FilmBox>
+            <img className="filmimg" src="https://user-images.githubusercontent.com/87605663/159729602-0c58d5c1-48ab-4477-a9c2-e5495758906f.png" alt='film'/>
+            <h3 className="filmtitle">후지400</h3>
+            <p className="filminfo">필름설명이 필요한곳 입니다.</p>
+          </FilmBox>
+          <FilmBox>
+            <img className="filmimg" src="https://user-images.githubusercontent.com/87605663/159730302-7e8be631-9192-4c3e-8986-ef3f1dc76cd1.png" alt='film'/>
+            <h3 className="filmtitle">흑백필름</h3>
+            <p className="filminfo">필름설명이 필요한곳 입니다.</p>
+          </FilmBox>
+        </Section>
       </Container>
     </>
   );
@@ -22,10 +43,36 @@ const Container = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   text-align: center;
-
+  align-items: center;
 `;
 
 const Section = styled.section`
+  width: 60vw;
+  height: 100%;
+  /* border: 1px solid blue; */
+  display: flex;
+  justify-content: space-around;
+  margin-top: 5rem;
+`;
 
-`
+
+
+const FilmBox = styled.div`
+  width: 20vw;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > h3.filmtitle {
+    margin-top: 10px;
+  }
+  > img.filmimg {
+    height: 15vh;
+    width: 10vw;
+  }
+  > p.filminfo {
+    font-size: 13px;
+  }
+`;
