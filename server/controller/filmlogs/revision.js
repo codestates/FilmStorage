0982f674ae4f948d55e0d373cb4e5c55267df41f
ssankq <ssankq@gmail.com>
2022-03-28 15:45:42 +0900

    [Back]_Feat_필름로그 수정 및 이미지 삽입 기능 구현

--- server/controller/filmlogs/revision.js
+++ server/controller/filmlogs/revision.js
@@ -2,41 +2,67 @@ require("dotenv").config();
 const { filmlogs } = require("../../models");
 
 module.exports = {
-    patch: async (req, res) => {
-        res.send("hello world");
-    },
-    photo: async (req, res) => {
-        const { user_id, filmlog_id } = req.params;
+  patch: async (req, res) => {
+    const { user_id, filmlog_id } = req.params;
 
-        try {
-            await filmlogs.update(
-                {
-                    photo: `${process.env.DOMAIN}/filmlogs/photos/${req.file.filename}`,
-                },
-                {
-                    where: {
-                        id: filmlog_id,
-                        user_id: user_id,
-                    },
-                }
-            );
+    try {
+      const { filmtype, contents } = req.body;
 
-            const getUpdatedFilmLogInfo = await filmlogs.findOne({
-                where: {
-                    id: filmlog_id,
-                    user_id: user_id,
-                },
-            });
-
-            res.status(200).json({
-                message: "Photo has been updated",
-                data: getUpdatedFilmLogInfo.dataValues.photo,
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({
-                message: "Internal Server Error",
-            });
+      await filmlogs.update(
+        {
+          filmtype,
+          contents,
+        },
+        {
+          where: {
+            id: filmlog_id,
+            user_id,
+          },
         }
-    },
+      );
+
+      res.status(200).send({
+        message: "Successfully Modified",
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({
+        message: "Internal Server Error",
+      });
+    }
+  },
+  photo: async (req, res) => {
+    const { user_id, filmlog_id } = req.params;
+
+    try {
+      await filmlogs.update(
+        {
+          photo: `${process.env.DOMAIN}/filmlogs/photos/${req.file.filename}`,
+        },
+        {
+          where: {
+            id: filmlog_id,
+            user_id: user_id,
+          },
+        }
+      );
+
+      const getUpdatedFilmLogInfo = await filmlogs.findOne({
+        where: {
+          id: filmlog_id,
+          user_id: user_id,
+        },
+      });
+
+      res.status(200).json({
+        message: "Photo has been updated",
+        data: getUpdatedFilmLogInfo.dataValues.photo,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        message: "Internal Server Error",
+      });
+    }
+  },
 };
