3c784d162b95ae6fbeb92182d04f98b52fbfa95c
ssankq <ssankq@gmail.com>
2022-03-28 15:46:11 +0900

    [Back]_Feat_좋아요 카운트업 기능 구현

--- server/controller/filmlogs/likes.js
+++ server/controller/filmlogs/likes.js
@@ -1,49 +1,85 @@
-const { likes } = require("../../models")
+const { likes, filmlogs } = require("../../models");
 
 module.exports = {
-    post: async (req, res) => {
-        const { user_id, filmlog_id } = req.params
-        // res.send('hello world')
-        try {
-            const [likeData, created] = await likes.findOrCreate({
-                where: {
-                    user_id,
-                    filmlog_id,
-                }
-            })
-            //like : true 생성
-            // 한 번 다시 클릭 created가 있으니까 취소 됨
-            // 다시 클릭하면(like : false) 조건에 맞지 않으니 새로운 레코드 생성(like : true)
-            //초기 like 값 1
-            if (!created) {
-                if (likeData.dataValues.like === true) {
-                    await likes.update({ like: false }, {
-                        where: {
-                            user_id,
-                            filmlog_id,
-                        }
-                    })
-                } else { // like === 0
-                    await likes.update({ like: true }, {
-                        where: {
-                            user_id,
-                            filmlog_id
-                        }
-                    })
-                }
+  post: async (req, res) => {
+    const { user_id, filmlog_id } = req.params;
+    // res.send('hello world')
+    try {
+      const [likeData, created] = await likes.findOrCreate({
+        where: {
+          user_id,
+          filmlog_id,
+        },
+      });
+      //like : true 생성
+      // 한 번 다시 클릭 created가 있으니까 취소 됨
+      // 다시 클릭하면(like : false) 조건에 맞지 않으니 새로운 레코드 생성(like : true)
+      //초기 like 값 1
+      if (created) {
+        await filmlogs.increment(
+          { likesCount: 1 },
+          {
+            where: {
+              id: filmlog_id,
+            },
+          }
+        );
+      }
+      if (!created) {
+        if (likeData.dataValues.like === true) {
+          await likes.update(
+            { like: false },
+            {
+              where: {
+                user_id,
+                filmlog_id,
+              },
             }
-            const getLikeData = await likes.findOne({
-                where: {
-                    user_id,
-                    filmlog_id
-                }
-            })
-            res.status(200).json({ message: "ok", data: getLikeData.dataValues })
-
-
-        } catch (err) {
-            res.status(500).send({ message: "internal server error" })
-            console.log(err)
+          );
+          await filmlogs.increment(
+            { likesCount: -1 },
+            {
+              where: {
+                id: filmlog_id,
+              },
+            }
+          );
+        } else {
+          // like === 0
+          await likes.update(
+            { like: true },
+            {
+              where: {
+                user_id,
+                filmlog_id,
+              },
+            }
+          );
+          await filmlogs.increment(
+            { likesCount: 1 },
+            {
+              where: {
+                id: filmlog_id,
+              },
+            }
+          );
         }
+      }
+      const getLikeData = await likes.findOne({
+        where: {
+          user_id,
+          filmlog_id,
+        },
+      });
+      res.status(200).json({
+        message: "ok",
+        data: getLikeData.dataValues,
+      });
+    } catch (err) {
+      res.status(500).send({
+        message: "internal server error",
+      });
+      console.log(err);
     }
-}
+  },
+};
