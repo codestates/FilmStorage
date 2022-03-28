829d8722fe25de148f60958028d744b375abc57d
ssankq <ssankq@gmail.com>
2022-03-28 15:47:01 +0900

    [Back]_Feat_조건에 따른 DB 데이터 조회 기능 구현

--- server/controller/filmlogs/topthree.js
+++ server/controller/filmlogs/topthree.js
@@ -1,51 +1,80 @@
-const { filmlogs } = require('../../models')
-const { Op } = require('sequelize');
+const { filmlogs } = require("../../models");
+const { Op } = require("sequelize");
 
 module.exports = {
-    get: async (req, res) => {
-        try {
-            const today = new Date();
-            const startDay = today.getDate() - today.getDay() + 1;
-            const startOfWeek = new Date(
-                today.getFullYear(),
-                today.getMonth(),
-                startDay
-            );
-            // const endOfWeek = new Date(
-            //     today.getFullYear(),
-            //     today.getMonth(),
-            //     endDay,
-            //     23,
-            //     59,
-            //     59
-            // )
-            const endOfWeek = new Date(
-                2022, 02, 25, 14, 33
-            )
-            console.log(startOfWeek.toLocaleString(), endOfWeek.toLocaleString())
-            //날짜를 업데이트 하기
-            // const startDay  = today.getDate() - today.getDay() + 1;
-            // const endDay = startDay + 6;
-            // const startOfWeek = new Date(
-            //     today.getFullYear(),
-            //     today.getMonth(),
-            //     startDay
-            // );
-            // const endOfWeek = new Date(
-            //     today.getFullYear()
-            // )
-            const filmlogData = await filmlogs.findAll({
-                where: {
-                    createdAt: {
-                        [Op.between]: [startOfWeek, endOfWeek]
-                    },
-                },
-            })
-            res.status(200).json({
-                data: getWeeklyTopThree,
-            })
-        } catch (err) {
-            console.log(err)
-        }
+  get: async (req, res) => {
+    try {
+      const today = new Date();
+      const startDay = today.getDate() - today.getDay() + 1;
+      const endDay = startDay + 6;
+      const startOfWeek = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        startDay
+      );
+      const endOfWeek = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        endDay,
+        23,
+        59,
+        59
+      );
+
+      const topOne = await filmlogs.max("views", {
+        where: {
+          createdAt: {
+            [Op.between]: [startOfWeek, endOfWeek],
+          },
+        },
+      });
+      const topTwo = await filmlogs.max("views", {
+        where: {
+          views: {
+            [Op.lt]: topOne,
+          },
+          createdAt: {
+            [Op.between]: [startOfWeek, endOfWeek],
+          },
+        },
+      });
+      const topthree = await filmlogs.max("views", {
+        where: {
+          views: {
+            [Op.lt]: topTwo,
+          },
+          createdAt: {
+            [Op.between]: [startOfWeek, endOfWeek],
+          },
+        },
+      });
+
+      const getTopThreeData = await filmlogs.findAll({
+        where: {
+          views: {
+            [Op.or]: [topOne, topTwo, topthree],
+          },
+          createdAt: {
+            [Op.between]: [startOfWeek, endOfWeek],
+          },
+        },
+      });
+
+      const topThreeData = getTopThreeData.map((data) => {
+        const { id, user_id, photo, filmtype } = data;
+				return {
+					id, user_id, photo, filmtype
+				}
+      });
+
+      res.status(200).json({
+        data: topThreeData,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        message: "Internal Server Error",
+      });
     }
-}
+  },
+};
--- server/controller/filmlogs/total.js
+++ server/controller/filmlogs/total.js
@@ -1,5 +1,35 @@
+const { filmlogs } = require("../../models");
+
 module.exports = {
-    get: async (req, res) => {
-        res.send('hello world')
+  get: async (req, res) => {
+    try {
+      const totalInfo = await filmlogs.findAll();
+
+      if (!totalInfo) {
+        res.status(404).send({
+          message: "Failed to load information",
+        });
+      } else {
+        const filteredInfo = totalInfo.map((data) => {
+          const { id, user_id, photo, filmtype } = data;
+          return {
+            id,
+            user_id,
+            photo,
+            filmtype,
+          };
+        });
+
+        res.status(200).json({
+          message: "ok",
+          data: filteredInfo,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({
+        message: "Internal Server Error",
+      });
     }
-}
+  },
+};
