0f07001c655ff89416a785e278836938bf932568
ssankq <ssankq@gmail.com>
2022-03-28 17:59:54 +0900

    [Back]_Comment_Router에 대한 주석 추가 및 정리

--- server/controller/filmlogs/deletion.js
+++ server/controller/filmlogs/deletion.js
@@ -1,5 +1,23 @@
+const { filmlogs } = require("../../models");
+
 module.exports = {
   delete: async (req, res) => {
-    res.send('hello world')
-  }
-}
+    const { filmlog_id } = req.params;
+    try {
+      await filmlogs.destroy({
+        where: {
+          id: filmlog_id,
+        },
+      });
+
+      res.status(200).send({
+        message: "Successfully Deleted",
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({
+        message: "Internal Server Error",
+      });
+    }
+  },
+};
