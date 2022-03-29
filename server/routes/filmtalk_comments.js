509972f038d46a4b58b0a2d0eae0df86b7dbe0c4
ssankq <ssankq@gmail.com>
2022-03-29 09:18:15 +0900

    [Back]_Feat_필름토크 CRUD에 따른 라우트 조정

--- server/routes/filmtalk_comments.js
+++ server/routes/filmtalk_comments.js
@@ -3,9 +3,9 @@ const filmtalk_commentsRouter = express.Router();
 
 const { filmtalk_commentsController } = require("../controller");
 
-filmtalk_commentsRouter.post("/register/:user_id", filmtalk_commentsController.register.post);
-filmtalk_commentsRouter.patch("/revision", filmtalk_commentsController.revision.patch);
-filmtalk_commentsRouter.delete("/deletion", filmtalk_commentsController.deletion.delete);
-filmtalk_commentsRouter.get("/total/:board_id", filmtalk_commentsController.total.get);
+filmtalk_commentsRouter.post("/register/:user_id/:filmtalk_id", filmtalk_commentsController.register.post);
+filmtalk_commentsRouter.patch("/revision/:user_id/:filmtalk_id/:filmtalk_comments_id", filmtalk_commentsController.revision.patch);
+filmtalk_commentsRouter.delete("/deletion/:user_id/:filmtalk_id/:filmtalk_comments_id", filmtalk_commentsController.deletion.delete);
+filmtalk_commentsRouter.get("/total/:filmtalk_id", filmtalk_commentsController.total.get);
 
 module.exports = filmtalk_commentsRouter;
