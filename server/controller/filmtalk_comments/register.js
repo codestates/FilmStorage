325fbdbd0a6b5741c748d5e1302238e9f3f6e9f3
ssankq <ssankq@gmail.com>
2022-03-29 09:16:15 +0900

    [Back]_Feat_필름토크 댓글 CRUD 구현

--- server/controller/filmtalk_comments/register.js
+++ server/controller/filmtalk_comments/register.js
@@ -1,5 +1,25 @@
+const { filmtalk_comments } = require("../../models");
+
 module.exports = {
   post: async (req, res) => {
-    res.send('hello world')
-  }
-}
+    try {
+      const { user_id, filmtalk_id } = req.params;
+      const { contents } = req.body;
+
+      await filmtalk_comments.create({
+        user_id,
+        filmtalk_id,
+        contents,
+      });
+
+      res.status(201).send({
+        message: "Successfully registered",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        message: "Internal Server Error",
+      });
+    }
+  },
+};
--- server/controller/filmtalk_comments/revision.js
+++ server/controller/filmtalk_comments/revision.js
@@ -1,5 +1,32 @@
+const { filmtalk_comments } = require("../../models");
+
 module.exports = {
   patch: async (req, res) => {
-    res.send('hello world')
-  }
-}
+    try {
+      const { user_id, filmtalk_id, filmtalk_comments_id } = req.params;
+      const { contents } = req.body;
+
+      await filmtalk_comments.update(
+        {
+          contents,
+        },
+        {
+          where: {
+            id: filmtalk_comments_id,
+            user_id,
+            filmtalk_id,
+          },
+        }
+      );
+
+      res.status(200).send({
+        message: "Successfully modified",
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({
+        message: "Internal Server Error",
+      });
+    }
+  },
+};
