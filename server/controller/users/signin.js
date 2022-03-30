43ff0839450c5c7d78131f849d632859dba17b53
ssankq <ssankq@gmail.com>
2022-03-30 17:55:20 +0900

    [Back]_Feat_카카오 소셜 로그인 구현

--- server/controller/users/signin.js
+++ server/controller/users/signin.js
@@ -1,36 +1,42 @@
-const { users } = require('../../models');
-const { sign } = require('jsonwebtoken');
+const { users } = require("../../models");
+const { sign } = require("jsonwebtoken");
+const axios = require("axios");
 require("dotenv").config();
 
 module.exports = {
   post: async (req, res) => {
-    const { email, password } = req.body
+    const { email, password } = req.body;
     if (!email || !password) {
-      res.status(400).send({ message: "Invalid email or password" })
+      res.status(400).send({ message: "Invalid email or password" });
     } else {
       try {
         const userData = await users.findOne({
           where: {
             email,
-            password
-          }
-        })
+            password,
+          },
+        });
         if (!userData) {
-          res.status(404).send({ message: "No matching user information" })
+          res.status(404).send({ message: "No matching user information" });
         } else {
-          console.log('유저정보#######>>>', userData.dataValues)
-          const accessToken = sign(userData.dataValues, process.env.ACCESS_SECRET, {
-            expiresIn: 1000 * 60 * 60 * 2
-          })
+          // console.log("유저정보#######>>>", userData.dataValues);
+          const accessToken = sign(
+            userData.dataValues,
+            process.env.ACCESS_SECRET,
+            {
+              expiresIn: 1000 * 60 * 60 * 2,
+            }
+          );
 
-          res.status(200)
+          res
+            .status(200)
             .cookie("accessToken", accessToken, {
               sameSite: "none",
               domain: "localhost",
               path: "/",
               secure: true,
               httpOnly: true,
-              maxAge: 1000 * 60 * 60 * 2
+              maxAge: 1000 * 60 * 60 * 2,
             })
             .send({
               data: accessToken,
@@ -42,5 +48,71 @@ module.exports = {
         res.status(500).send({ message: "Internal Server Error" });
       }
     }
-  }
-}
+  },
+
+  kakao: async (req, res) => {
+    // 인가 코드가 잘 받아와졌는지 확인
+    // console.log(req.query);
+    try {
+      const oauthData = {
+        grant_type: "authorization_code",
+        client_id: process.env.KAKAO_REST_API_KEY,
+        redirect_uri: process.env.KAKAO_REDIRECT_URI,
+        code: req.query.code,
+      };
+      const url = `https://kauth.kakao.com/oauth/token?code=${oauthData.code}&client_id=${oauthData.client_id}&redirect_uri=${oauthData.redirect_uri}&grant_type=${oauthData.grant_type}`;
+
+      const response = await axios.post(
+        url,
+        {},
+        {
+          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+        }
+      );
+
+      const { access_token } = response.data;
+      const getKakaoUserInfo = await axios.get(
+        `https://kapi.kakao.com/v2/user/me`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+            "Content-type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+
+      const { profile, email } = getKakaoUserInfo.data.kakao_account;
+
+      let userInfo = await users.findOne({ where: { email } });
+
+      if (!userInfo) {
+        userInfo = await users.create({
+          email: email,
+          profile: profile.profile_image_url,
+          nickname: profile.nickname,
+        });
+      }
+
+      const accessToken = sign(userInfo.dataValues, process.env.ACCESS_SECRET, {
+        expiresIn: 1000 * 60 * 60 * 2,
+      });
+
+      res
+        .status(200)
+        .cookie("accessToken", accessToken, {
+          sameSite: "none",
+          domain: "localhost",
+          path: "/",
+          secure: true,
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 2,
+        })
+        .send({
+          message: "Logged in with your Kakao account",
+        });
+    } catch (err) {
+      console.error(err);
+      // req;
+    }
+  },
+};
