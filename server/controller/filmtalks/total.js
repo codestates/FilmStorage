c37633938b8cdc490141e6346e9ea93743edc2a5
ssankq <ssankq@gmail.com>
2022-04-01 22:14:34 +0900

    [Back]_Feat_필름토크 페이지네이션에 맞춰 코드 수정

--- server/controller/filmtalks/total.js
+++ server/controller/filmtalks/total.js
@@ -1,26 +1,45 @@
-const { filmtalks } = require("../../models");
+const { filmtalks, users } = require("../../models");
 
 module.exports = {
   get: async (req, res) => {
-    const { offset, limit } = req.query;
-    if (!offset || !limit) {
-      res.status(404).send({ message: "Not Found" })
-    } else {
-      try {
-        const talks = await filmtalks.findAll({
-          order: [["createdAt", "DESC"],
-          ["id", "DESC"]],
+    const { offset } = req.query;
+    try {
+      let talks;
+      if (!offset) {
+        talks = await filmtalks.findAll({
+          order: [
+            ["createdAt", "DESC"],
+            ["id", "DESC"],
+          ],
+          include: [{ model: users }],
+        });
+      } else {
+        talks = await filmtalks.findAll({
+          order: [
+            ["createdAt", "DESC"],
+            ["id", "DESC"],
+          ],
           offset: Number(offset),
-          limit: Number(limit)
-        },
+          include: [{ model: users }],
+        });
+      }
+      
+      const filmTalkData = talks.map((data) => {
+        const { id, category, createdAt, title, views } = data;
+        return {
+          id,
+          category,
+          createdAt,
+          title,
+          views,
+          nickname: data.user.nickname,
+        };
+      });
 
-        )
-        res.status(200).send({ message: "ok", data: [talks] })
-      }
-      catch (err) {
-        console.log(err)
-        res.status(500).send({ message: "Internal Server Error" })
-      }
+      res.status(200).send({ message: "ok", data: filmTalkData });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ message: "Internal Server Error" });
     }
-  }
-}
+  },
+};
