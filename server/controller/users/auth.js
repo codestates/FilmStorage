5c67b0da7c5802dd8521ff083d9c148588457154
ssankq <ssankq@gmail.com>
2022-04-01 15:23:54 +0900

    [Back]_Comments_안쓰는 주석 삭제

--- server/controller/users/auth.js
+++ server/controller/users/auth.js
@@ -3,15 +3,8 @@ const { verify } = require("jsonwebtoken")
 require("dotenv").config();
 
 
-//{
-// "email" : "boo1996@naver.com"   
-//}
-
 module.exports = {
   get: async (req, res) => {
-    // console.log("헤더에 담긴 쿠키", req.headers.authorization.split("Bearer ")[1]);
-    // console.log("쿠키스토리지에 담긴 쿠키", req.cookies.accessToken);
-    // console.log(req.headers.authorization)
     if (!req.cookies.accessToken) {
       res.status(401).json({
         data: null,
@@ -20,9 +13,7 @@ module.exports = {
     } else {
       try {
         const accessToken = req.cookies.accessToken
-        console.log("클라이언트에서 보낸 토큰", accessToken)
         const decoded = verify(accessToken, process.env.ACCESS_SECRET);
-        console.log("해독된 토큰 정보", decoded)
         const userData = await users.findOne({
           where: {
             email: decoded.email
@@ -35,7 +26,6 @@ module.exports = {
             data: userData.dataValues,
             message: "ok"
           })
-          console.log("토큰을 해독한 후 얻은 유저정보###>", userData)
         }
       } catch (err) {
         console.log(err);
