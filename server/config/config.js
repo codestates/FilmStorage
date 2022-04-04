dc01b1fdbe9f5cabd7a33b2a0e11be219f73cb54
LeeHyeongeol <leehyeongul@gmail.com>
2022-04-04 11:15:52 +0900

    [Deploy]_Fix_script,appspec.yml,buildspec.yml파일 추가

--- server/config/config.js
+++ server/config/config.js
@@ -20,13 +20,7 @@ module.exports = {
       acquire: 30000,
       idle: 10000
     }
-    // [출처][nodejs - sequelize] timezone 적용, 조회시 시간 제대로 표기하기| 작성자 demonic
 
-
-    // "define": {
-    //   timestamps: true,
-    //   supportBigNumbers: true,
-    // }
   },
   "test": {
     "username": "root",
@@ -36,12 +30,25 @@ module.exports = {
     "dialect": "mysql",
     "timezone": "+09: 00"
   },
+
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
     "dialect": "mysql",
-    "timezone": "+09: 00"
+    "timezone": "+09: 00",
+    "dialectOptions": {
+      charset: 'utf8mb4',
+      dateStrings: true,
+      typeCast: true
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+
   }
 }
