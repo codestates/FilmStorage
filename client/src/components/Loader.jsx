942086fe329f8bbde654afc7884b162c50d7da36
jeongdxxn <dxxn.jeong@gmail.com>
2022-04-07 09:28:45 +0900

    [Front]_Feat_loader 이미지 변경

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -11,48 +11,55 @@ const LoaderWrap = styled.div`
   align-items: center;
 `;
 
+const Img = styled.img`
+  /* border: 1px solid red; */
+  width: 100px;
+  padding: 100px;
+`
+
 // Here we create a component that will rotate everything we pass in over two seconds
-const Rotate = styled.div`
-  position: relative;
-  color: lightgray;
-  font-size: 2em;
-  letter-spacing: 5px;
-  /* border-bottom: 16px solid lightgray; */
-  line-height: 1.4;
-  font-family: serif;
-  font-family: 'Pacifico', cursive;
+// const Rotate = styled.div`
+//   position: relative;
+//   color: lightgray;
+//   font-size: 2em;
+//   letter-spacing: 5px;
+//   /* border-bottom: 16px solid lightgray; */
+//   line-height: 1.4;
+//   font-family: serif;
+//   font-family: 'Pacifico', cursive;
 
-  &:before {
-    content: attr(data-text);
-    color: tomato;
-    overflow: hidden;
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 3em;
-    width: 100%;
-    /* border-bottom: 16px solid tomato; */
-    animation: colorChange 1s linear infinite;
-    overflow: hidden;
-  }
+//   &:before {
+//     content: attr(data-text);
+//     color: tomato;
+//     overflow: hidden;
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     height: 3em;
+//     width: 100%;
+//     /* border-bottom: 16px solid tomato; */
+//     animation: colorChange 1s linear infinite;
+//     overflow: hidden;
+//   }
 
-  @keyframes colorChange {
-    0% {
-      width: 0;
-    }
-    100% {
-      width: 100%;
-    }
-  }
-`;
+//   @keyframes colorChange {
+//     0% {
+//       width: 0;
+//     }
+//     100% {
+//       width: 100%;
+//     }
+//   }
+// `;
 
 const Loader = () => {
   return (
     <LoaderWrap>
       {/* <ReactLoading type="spin" color="tomato" /> */}
-      <Rotate data-text="FilmStorage...">
+      {/* <Rotate data-text="FilmStorage...">
         FilmStorage...
-      </Rotate>
+      </Rotate> */}
+      <Img src="https://user-images.githubusercontent.com/87605663/161916389-98a1c6d1-e050-48ca-87c5-2cec767de25c.gif"/>
     </LoaderWrap>
   );
 };
