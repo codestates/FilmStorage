998643ca4ac6418f421c250f6ba049d3445a6667
SEB_Full_36기_서외구 <89363048+sud665@users.noreply.github.com>
2022-04-07 09:18:41 +0900

    [Front]_Fix_필름취향찾기 기능 수정

---
+++ client/src/components/findingfilmtype/FilmTypeTestData.jsx
@@ -0,0 +1,52 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowArray": true}] */
+
+export default [
+  [
+    [
+      "흑백",
+      "https://user-images.githubusercontent.com/89363048/161432068-1fef271b-8e31-4e53-b6d8-20f6fd82b57a.jpeg",
+    ],
+
+    [
+      "칼러",
+      "https://user-images.githubusercontent.com/87605663/160955300-7963c2f2-ff03-4d7d-8bdb-f132c5aabf35.JPG",
+    ],
+
+    [
+      "기타",
+      "https://user-images.githubusercontent.com/87605663/160956248-734d6fd4-7631-471e-b5b3-a66ca2e25143.JPG",
+    ],
+  ],
+  [
+    [
+      "흑백인물",
+      "https://user-images.githubusercontent.com/87605663/160955665-178f0031-5b00-4d18-987c-0cc71455c629.JPG",
+    ],
+    [
+      "흑백풍경",
+      "https://user-images.githubusercontent.com/87605663/160955958-30c00cd1-a2e5-49ba-b3a1-a643682fa0fd.JPG",
+    ],
+  ],
+
+  [
+    [
+      "흑백인물실내",
+      "https://user-images.githubusercontent.com/87605663/160956219-de688f20-bffb-4e77-957d-5c6ea690bbdb.JPG",
+    ],
+    [
+      "흑백인물실외",
+      "https://user-images.githubusercontent.com/87605663/160956648-5d06eadf-5c8b-455a-8cf6-487965f372a8.JPG",
+    ],
+  ],
+
+  [
+    [
+      "흑백인물실내밝음",
+      "https://user-images.githubusercontent.com/87605663/160957215-574748e1-2693-4659-85b0-2816da24a856.JPG",
+    ],
+    [
+      "흑백인물실내어둠",
+      "https://user-images.githubusercontent.com/87605663/160956749-7dbd2485-95fe-490f-8d2c-169c906e529c.JPG",
+    ],
+  ],
+];
