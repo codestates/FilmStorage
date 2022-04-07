445177babace205ceb14243f24ddff19b05118ee
ssankq <ssankq@gmail.com>
2022-04-07 10:04:59 +0900

    [Back]_Fix_모델 기본값 설정 반영되지않던점 수정

--- server/models/users.js
+++ server/models/users.js
@@ -16,9 +16,16 @@ module.exports = (sequelize, DataTypes) => {
       email: DataTypes.STRING,
       password: DataTypes.STRING,
       nickname: DataTypes.STRING,
-      profile: DataTypes.STRING,
+      profile: {
+        type: DataTypes.STRING,
+        defaultValue:
+          "https://user-images.githubusercontent.com/89354370/161719211-8d182204-a775-417e-9165-399fa6df48ef.png",
+      },
       mobile: DataTypes.STRING,
-      kakaouser: DataTypes.BOOLEAN,
+      kakaouser: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
     },
     {
       sequelize,
