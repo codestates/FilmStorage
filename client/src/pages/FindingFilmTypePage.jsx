052a8ce1359fd78bb7a9d7a30c4ce112abf90ba4
SEB_Full_36기_서외구 <89363048+sud665@users.noreply.github.com>
2022-04-10 03:23:46 +0900

    [Front]_Refact_필름취향테스트 디자인 수정

--- client/src/pages/FindingFilmTypePage.jsx
+++ client/src/pages/FindingFilmTypePage.jsx
@@ -37,9 +37,6 @@ export default function FindingFilmTypePage() {
   // 테스트그림들 나열하기
   const [filmTypeList, setFilmTypeList] = useState([]);
 
-  // 클릭시 선택된 이미지 형제 태그
-  const [selectedImg, setSelectedImg] = useState("");
-
   // 이미지 선택 정해지기
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -164,8 +161,14 @@ export default function FindingFilmTypePage() {
       setResultForRender({});
       setFilmTypeList([]);
       setFilmTypeTest(copyFilmTypeTestData);
+      handleReplay();
     };
   }, []);
+
+  // 다시하기 버튼 기능 함수
+  const handleReplay = () => {
+    window.location.reload();
+  };
 
   return (
     <>
@@ -176,7 +179,9 @@ export default function FindingFilmTypePage() {
         </ProceedContainer>
         {isStart ? (
           <>
-            <ImgContainer>마음에 드는 사진 3장씩 선택해주세요.</ImgContainer>
+            <ImgContainer>
+              <h3>마음에 드는 사진을 3장씩 선택해주세요.</h3>
+            </ImgContainer>
             <StartButtonBox>
               <Button
                 onClick={() => {
@@ -194,11 +199,7 @@ export default function FindingFilmTypePage() {
                 {filmTypeList.map((test, idx) => {
                   return (
                     <BlockBox key={idx}>
-                      <ImgSelectedBox
-                        style={{
-                          diplay: selectedImg === idx ? "block" : "hidden",
-                        }}
-                      ></ImgSelectedBox>
+                      <ImgSelectedBox></ImgSelectedBox>
                       <ImgBox
                         src={test.src}
                         alt={`${test.iso}&${test.type}&${test.company}`}
@@ -216,31 +217,43 @@ export default function FindingFilmTypePage() {
                   {isLoadResult ? (
                     <Loader />
                   ) : (
-                    <FilmBox>
-                      <img
-                        className="filmimg"
-                        src={resultForRender.imglink}
-                        alt="film"
-                      />
-                      <h3 className="filmtitle">{resultForRender.filmname}</h3>
-                      <div className="filminfo-box">
-                        <span className="filminfo">
-                          <span className="bold">필름 타입</span>{" "}
-                          {resultForRender.type} |
-                        </span>
-                        <span className="filminfo">
-                          <span className="bold">촬영 횟수</span>{" "}
-                          {resultForRender.shots} |
-                        </span>
-                        <span className="filminfo">
-                          <span className="bold">감도</span>ISO
-                          {resultForRender.iso}
-                        </span>
-                      </div>
-                      <span className="filminfo-text">
-                        {resultForRender.content}
-                      </span>
-                    </FilmBox>
+                    <DropDown>
+                      <ImgContainer>
+                        <h3>테스트 결과</h3>
+                      </ImgContainer>
+                      <FilmBox>
+                        <img
+                          className="filmimg"
+                          src={resultForRender.imglink}
+                          alt="film"
+                        />
+                        <h3 className="filmtitle">
+                          {resultForRender.filmname}
+                        </h3>
+                        <div className="filminfo-box">
+                          <span className="filminfo">
+                            <span className="bold">필름 타입</span>{" "}
+                            {resultForRender.type} |
+                          </span>
+                          <span className="filminfo">
+                            <span className="bold">촬영 횟수</span>{" "}
+                            {resultForRender.shots} |
+                          </span>
+                          <span className="filminfo">
+                            <span className="bold">감도 ISO</span>
+                            {resultForRender.iso}
+                          </span>
+                        </div>
+                        <ResultComment>{resultForRender.content}</ResultComment>
+                        <Button
+                          onClick={() => {
+                            handleReplay();
+                          }}
+                        >
+                          다시하기
+                        </Button>
+                      </FilmBox>
+                    </DropDown>
                   )}
                 </ImgContainer>
               </>
@@ -293,12 +306,14 @@ const Dot = styled.div`
 
 const ImgContainer = styled.div`
   display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
 `;
 
 const StartButtonBox = styled.div`
   width: 30rem;
   height: 10rem;
-  margin-top: 80px;
+
   text-align: center;
 `;
 
@@ -311,7 +326,7 @@ const GridContainer = styled.div`
 `;
 
 const FilmBox = styled.div`
-  width: 20vw;
+  width: 40vw;
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -321,13 +336,15 @@ const FilmBox = styled.div`
     /* border: 1px solid red; */
   }
   > img.filmimg {
-    height: 15vh;
+    width: 200px;
+    height: 200px;
     /* width: 10vw; */
     object-fit: cover;
+    cursor: pointer;
   }
 
   > div.filminfo-box {
-    /* border: 1px solid red; */
+    margin: 20px 20px 20px 20px;
     > span.filminfo {
       /* border: 1px solid red; */
       padding: 2px;
@@ -340,7 +357,7 @@ const FilmBox = styled.div`
   > .filminfo-text {
     border: 1px solid Gainsboro;
     border-radius: 20px;
-    margin: 10px;
+    margin: 20px;
     padding: 40px;
     font-size: 14px;
   }
@@ -390,4 +407,28 @@ const ImgSelectedBox = styled.div`
   border-radius: 20px;
   cursor: pointer;
   opacity: 0.6;
+`;
+
+const DropDown = styled.div`
+  /* border: 1px solid red; */
+  position: relative;
+`;
+
+const ResultComment = styled.div`
+  /* border: 1px solid red; */
+  display: none;
+  ${DropDown}:hover & {
+    display: block;
+  }
+  position: absolute;
+  right: -20px;
+  width: 200px;
+
+  margin: 10px;
+  padding: 10px;
+  background: #fff;
+  font-size: 14px;
+  border: 1px solid Gainsboro;
+  border-radius: 10px;
+  box-shadow: 5px 5px 10px Gainsboro;
 `;
