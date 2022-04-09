fe6c0a305a2feef6176c50bbd1356b3c7973117f
jeongdxxn <dxxn.jeong@gmail.com>
2022-04-09 15:47:54 +0900

    [Front]_Design_레이아웃 수정 완료

--- client/src/components/filmlog/FilmLogRevison.jsx
+++ client/src/components/filmlog/FilmLogRevison.jsx
@@ -83,14 +83,16 @@ export default function FilmLogRevison({
       <ModalBG onClick={() => setIsOpen()}>
         <ModalBox onClick={(e) => e.stopPropagation()}>
           <ModalNav>
-            <div className="navtitle">
-              <Button onClick={() => setIsOpen()}>
-                <FontAwesomeIcon icon={faAngleLeft} />
-              </Button>
+            <div className="nav-flex">
+              <FontAwesomeIcon
+                icon={faAngleLeft}
+                className="icon"
+                onClick={() => setIsOpen()}
+              />
             </div>
-            <div className="navtitle">사진 수정 하기</div>
-            <div className="navtitle">
-              <Button onClick={() => filmlogRevision()}>수정요청</Button>
+            <div className="nav-title">사진 수정 하기</div>
+            <div className="nav-flex">
+              <Button onClick={() => filmlogRevision()}>수정 완료</Button>
             </div>
           </ModalNav>
           <ModalImageBox>
@@ -118,19 +120,20 @@ export default function FilmLogRevison({
                 <div className="userinfo">{userInfo.nickname}</div>
               </UserInfo>
               <Tagarea>
-                필름선택
+                <Title>필름 선택</Title>
                 <FilmType setPhotoInfo={setMyPhotoInfo} photoInfo={photoInfo} />
               </Tagarea>
               <Textarea>
+                <Title>내용 입력</Title>
                 <textarea
                   className="filmcontent"
-                  placeholder="내용입력"
+                  placeholder="내용을 입력해 주세요"
                   value={myPhotoInfo.contents}
                   onChange={handleContents}
                 ></textarea>
               </Textarea>
               <LocationSearch>
-                <div className="searchTitle">장소선택</div>
+                <Title>장소선택</Title>
                 <Search>
                   <form onSubmit={handleSubmit}>
                     <input
@@ -162,8 +165,8 @@ const ModalBG = styled.div`
   z-index: 999;
   left: 0;
   top: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -173,153 +176,209 @@ const ModalBG = styled.div`
 const ModalBox = styled.div.attrs((props) => ({
   role: "dialog",
 }))`
-  width: 70vw;
-  height: 80vh;
+  /* border: 2px solid red; */
+  width: 1200px;
+  height: 650px;
   background: white;
   border-radius: 1rem;
   margin: 0;
+  overflow: hidden;
 `;
 
+// * Nav bar * //
 const ModalNav = styled.nav`
-  width: 70vw;
-  height: 10vh;
-  border-bottom: 1px solid black;
+  padding: 15px;
+  border-bottom: 1px solid gainsboro;
   display: flex;
-  font-size: 2rem;
-  text-align: center;
+  font-size: 22px;
+  align-items: center;
   justify-content: space-between;
-  > div.navtitle {
-    margin-top: 1.3rem;
-    margin-right: 2rem;
-    margin-left: 2rem;
+  box-sizing: border-box;
+  > div.nav-flex {
+  }
+  .nav-title {
+    /* border: 3px solid green; */
+    margin-left: 90px;
+  }
+  .icon {
+    margin-left: 10px;
+    cursor: pointer;
   }
 `;
 
+// * 전체 입력 부분 *//
 const ModalImageBox = styled.div`
   display: flex;
-  width: 70vw;
-  height: 70vh;
+  height: 100%;
+  box-sizing: border-box;
   /* border: 1px solid black; */
 `;
 
+// * 이미지 업로드(왼쪽)
 const ImageBox = styled.div`
-  width: 40vw;
-  border-right: 1px solid black;
+  flex: 1 500px;
+  height: 570px;
+  border-right: 1px solid gainsboro;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   text-align: center;
+  position: relative;
+  > img {
+    /* border: 1px solid green; */
+    width: 100%;
+    height: 100%;
+    border-bottom-left-radius: 1rem;
+    object-fit: cover;
+    position: absolute;
+    overflow: hidden;
+    z-index: 1;
+  }
   > div.imginsert {
-    margin-top: 15rem;
     margin-bottom: 1rem;
+  }
+  > .icon {
+    transition: 0.3s;
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    font-size: 32px;
+    z-index: 2;
+    color: #ffffffab;
+    cursor: pointer;
+    &:hover {
+      color: #222;
+    }
   }
 `;
 
 const Button = styled.button`
   padding: 10px 30px;
-  border: none;
+  margin: 5px;
+  border: 1px solid #444;
+  background: none;
+  color: #444;
   border-radius: 20px;
-  margin-left: 10px;
-  right: ${(props) => props.rigth || 0};
-  ${(props) => {
-    if (props.top) {
-      return css`
-        top: -50px;
-      `;
-    } else if (props.bottom) {
-      return css`
-        bottom: -50px;
-      `;
-    }
-  }}
+  font-family: "SCoreDream";
   font-size: 16px;
-  font-weight: 600;
+  font-weight: 500;
   cursor: pointer;
   &:hover {
     color: white;
     background: tomato;
+    border: 1px solid tomato;
     transition: 0.3s;
   }
 `;
 
+// * 내용 입력(오른쪽)
 const Content = styled.div`
+  flex: 1 500px;
+  height: 570px;
   display: flex;
+  padding: 20px;
   flex-direction: column;
+  box-sizing: border-box;
+  overflow: overlay;
+  z-index: 1;
 `;
 
+const Title = styled.div`
+  font-size: 14px;
+  color: #666;
+  padding: 10px 0;
+`;
+
+// * 유저 정보
 const UserInfo = styled.div`
-  /* border: 1px solid red; */
-  width: 30vw;
-  height: 10vh;
   display: flex;
-  > div.userinfo {
-    margin-top: 1.7rem;
-    margin-left: 1rem;
-    margin-bottom: 1.7rem;
-    /* border: 1px solid blue; */
-    font-size: 1.5rem;
-  }
-`;
-
-const Textarea = styled.div`
-  width: 30vw;
-  height: 30vh;
-  margin-left: 1rem;
-  margin-right: 1rem;
-  /* border: 1px solid blue; */
-  > textarea.filmcontent {
-    width: 29.5vw;
-    height: 29.5vh;
-    rows: "5";
-    cols: "30";
-    placeholder: "내용 입력";
-  }
-`;
-
-const Tagarea = styled.div`
-  width: 30vw;
-  margin-left: 1rem;
-  margin-bottom: 1rem;
-  /* border: 1px solid green; */
+  align-items: center;
+  padding-bottom: 10px;
+  margin-bottom: 5px;
+  border-bottom: 1px solid gainsboro;
 `;
 
 const UserImg = styled.img`
-  width: 40px;
-  height: 40px;
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
   border-radius: 20px;
-  border: 1px solid #eee;
-  background: #eee;
+  border: 2px solid #eee;
   object-fit: cover;
 `;
 
-const LocationArea = styled.div`
-  width: 30vw;
-  height: 100%;
-  margin: 1rem;
-  margin-top: 0;
+// * 필름 선택
+const Tagarea = styled.div`
+  padding-top: 20px;
+  z-index: 2;
 `;
 
-const LocationSearch = styled.div`
-  width: 30vw;
-  height: 30px;
-  display: flex;
-  /* justify-content: space-between; */
-  margin: 1rem;
-  margin-bottom: 0.5rem;
-  > div.searchTitle {
-    width: 5vw;
+// * 본문 입력
+const Textarea = styled.div`
+  > textarea.filmcontent {
+    box-sizing: border-box;
+    width: 100%;
+    height: 100px;
+    padding: 10px;
+    outline: none;
+    border: 1px solid gainsboro;
+    border-radius: 5px;
+    font-size: 14px;
+    line-height: 1.5em;
+    &::placeholder {
+      font-size: 14px;
+    }
+    &:focus {
+      box-shadow: 5px 5px 10px gainsboro;
+    }
   }
 `;
 
-const Search = styled.div`
-  /* border: 1px solid green; */
-  /* width: 100%; */
-  height: 100%;
+
+// * 장소 검색
+const LocationSearch = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between;
+  padding: 5px 0;
+  /* border: 1px solid blue; */
+`;
+
+
+const Search = styled.div`
+  input {
+    /* border: 1px solid green; */
+    width: 95%;
+    border: none;
+    border-bottom: 1px solid gainsboro;
+    outline: none;
+    transition: 0.2s;
+    padding: 10px;
+    font-size: 16px;
+    &:focus {
+      border-bottom: 1px solid #222;
+    }
+  }
 `;
 
 const ChoiceBox = styled.div`
-  border: 1px solid green;
-  width: 200px;
-  margin-left: 1rem;
+  width: 95%;
+  padding: 5px;
+  transition: 0.2s;
+  > .icon {
+    padding-left: 20px;
+    color: #999;
+    &:hover {
+      color: #444;
+    }
+  }
 `;
+
+const LocationArea = styled.div`
+  /* width: 30vw;
+  height: 100%;
+  margin: 1rem;
+  margin-top: 0; */
+`;
+
