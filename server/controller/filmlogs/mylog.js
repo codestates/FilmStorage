a0ced98cd6e628606b1b16537dc6974d2ebc5038
ssankq <ssankq@gmail.com>
2022-04-09 18:16:39 +0900

    [Back]_Feat_필름타입 필터 적용에 대한 응답 추가

--- server/controller/filmlogs/mylog.js
+++ server/controller/filmlogs/mylog.js
@@ -4,18 +4,29 @@ module.exports = {
   // 마이로그 조회 기능
   get: async (req, res) => {
     try {
-      const { offset, limit } = req.query;
+      const { offset, limit, filmtype } = req.query;
       const { user_id } = req.params;
       // 마이로그 무한스크롤 시 제공하는 데이터
-      const getMyLogData = await filmlogs.findAll({
-        where: { user_id },
-        offset: Number(offset),
-        limit: Number(limit),
-        order: [
-          ["createdAt", "DESC"],
-          ["id", "DESC"],
-        ],
-      });
+      let getMyLogData;
+      if (offset && limit) {
+        getMyLogData = await filmlogs.findAll({
+          where: { user_id },
+          offset: Number(offset),
+          limit: Number(limit),
+          order: [
+            ["createdAt", "DESC"],
+            ["id", "DESC"],
+          ],
+        });
+      } else if (filmtype) {
+        getMyLogData = await filmlogs.findAll({
+          where: { user_id, filmtype },
+          order: [
+            ["createdAt", "DESC"],
+            ["id", "DESC"],
+          ],
+        });
+      }
 
       if (!getMyLogData) {
         res.status(404).send({
@@ -32,8 +43,10 @@ module.exports = {
           };
         });
         // 응답하는 데이터 개수가 요청받은 개수(limit) 보다 적으면 end 메시지 응답
-        const message = getMyLogData.length < limit ? "end" : "ok";
-
+        let message = "ok"
+        if(limit) {
+          message = getMyLogData.length < limit ? "end" : "ok";
+        }
         res.status(200).json({
           message: message,
           data: myLogData,
